cc457621c684fd768c7838df31bb9324c1ff8543
Alexey Iuzhakov <iuzhakov.aleksei.ial@gmail.com>
2026-03-16 01:26:13 +0200

    Update build.gradle.kts. Remove dagger and kapt.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,51 +2,42 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.24"
-    kotlin("kapt") version "1.9.24"
-    id("org.jetbrains.compose") version "1.6.11"
+  kotlin("jvm") version "1.9.24"
+  id("org.jetbrains.compose") version "1.6.11"
 }
 
-group = "me.alex"
-version = "1.0"
+group = "com.thindie"
+version = "2.0"
 
 repositories {
-    google()
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+  google()
+  mavenCentral()
+  maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
-    val daggerVersion = "2.54"
-    implementation(compose.desktop.currentOs)
-    implementation("com.google.dagger:dagger:$daggerVersion")
-    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
-    @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-    implementation(compose.material3)
+  implementation(compose.desktop.currentOs)
+  @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+  implementation(compose.material3)
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+  kotlinOptions.jvmTarget = "17"
 }
 
-
-
-
-
 compose.desktop {
-    application {
-
-        buildTypes.release.proguard {
-            isEnabled.set(false)
-        }
-        mainClass = "ApplicationKt"
-        nativeDistributions {
-            vendor = "thindie"
-            copyright = "2023 thindie"
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "InvoicerMxl"
-            packageVersion = "1.0.0"
-            appResourcesRootDir.set(project.layout.projectDirectory.dir("src/appdata"))
-         }
-    }
+  application {
+	buildTypes.release.proguard {
+	  isEnabled.set(false)
+	}
+	mainClass = "Mainkt"
+	nativeDistributions {
+	  vendor = "thindie"
+	  copyright = "2026 thindie"
+	  targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+	  packageName = "InvoicerMxl"
+	  packageVersion = "2.0.0"
+	  appResourcesRootDir.set(project.layout.projectDirectory.dir("src/appdata"))
+	}
+  }
 }
